8abbe2c02bce21942f4309e8a3b5f6cd593b18b2
AlexanderdpS <adominguez@senati.pe>
2023-08-18 15:22:06 -0500

    Java Hello

---
+++ helloworld02/src/main/java/com/mycompany/helloworld02/Helloworld02.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworld02;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Helloworld02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Ola senatinos :)!");
+    }
+    
+}
