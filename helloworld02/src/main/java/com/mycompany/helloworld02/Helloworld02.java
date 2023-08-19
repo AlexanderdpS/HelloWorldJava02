7fad9a81e1869907f10e03be51f574109fa6f434
AlexanderdpS <adominguez@senati.pe>
2023-08-18 19:17:47 -0500

    Primer comit de nueva rama

--- helloworld02/src/main/java/com/mycompany/helloworld02/Helloworld02.java
+++ helloworld02/src/main/java/com/mycompany/helloworld02/Helloworld02.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.helloworld02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alumno
@@ -11,9 +13,25 @@ package com.mycompany.helloworld02;
 public class Helloworld02 {
 
     public static void main(String[] args) {
-        System.out.println("Alexander!");
-        System.out.println("Software Engineer :)!");
-        System.out.println("2023!");
+        System.out.println("INTRODUSCA EL PRIMER NUMERO: ");
+        Scanner valor1 = new Scanner(System.in);
+        int numero1=valor1.nextInt();
+        System.out.println("INTRODUSCA EL SEGUNDO NUMERO: ");
+        Scanner valor2 = new Scanner(System.in);
+        int numero2=valor2.nextInt();
+        
+        int suma= numero1 + numero2;
+        int resta= (numero1 - numero2);
+        int multiplicacion= numero1 * numero2;
+        double num1double = numero1;
+        double num2double = numero2;
+        double divicion= num1double / num2double;
+        
+        System.out.println("SUMA: " + suma);
+        System.out.println("RESTA: " + resta);
+        System.out.println("MULTIPLICACION: " + multiplicacion);
+        System.out.println("DIVICION: " + divicion);
+     
     }
     
 }
